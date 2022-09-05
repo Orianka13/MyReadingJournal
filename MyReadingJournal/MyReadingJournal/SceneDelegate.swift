50e8366df7e557919570a6d8a24c3a28d2d543da
Orianka13 <odobrogor@gmail.com>
2022-09-05 14:25:51 +0300

    Создан проект (убран storyboard)

---
+++ MyReadingJournal/MyReadingJournal/SceneDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  SceneDelegate.swift
+//  MyReadingJournal
+//
+//  Created by Олеся Егорова on 05.09.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        self.window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        self.window?.windowScene = windowScene
+        
+        let vc = ViewController()
+        let navVC = UINavigationController(rootViewController: vc)
+        
+        self.window?.rootViewController = navVC
+        self.window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+}
+
---
+++ MyReadingJournal/MyReadingJournal/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MyReadingJournal
+//
+//  Created by Олеся Егорова on 05.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
