088e7dfc9cda96c0fe285c1214ad514a9b5a9d14
Orianka13 <odobrogor@gmail.com>
2022-09-12 14:07:56 +0300

    Добавлено отображение модели на экран

---
+++ MyReadingJournal/MyReadingJournal/View/Onboarding/OnboardingCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OnboardingCollectionViewCell.swift
+//  MyReadingJournal
+//
+//  Created by Олеся Егорова on 12.09.2022.
+//
+
+import UIKit
+
+class OnboardingCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "Cell"
+    
+    @IBOutlet weak var slideImageView: UIImageView!
+    @IBOutlet weak var slideTitleLabel: UILabel!
+    @IBOutlet weak var slideDescriptionLabel: UILabel!
+    
+    func setup(_ slide: OnboardingSlide) {
+        self.slideImageView.image = slide.image
+        self.slideTitleLabel.text = slide.title
+        self.slideDescriptionLabel.text = slide.description
+        
+    }
+}
---
+++ MyReadingJournal/MyReadingJournal/View/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  MyReadingJournal
+//
+//  Created by Олеся Егорова on 05.09.2022.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private var slides = [OnboardingSlide]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+        self.slides = [OnboardingSlide(title: "Title1", description: "Description1", image: UIImage(named: "onb1") ?? UIImage()),
+                       OnboardingSlide(title: "Title2", description: "Description2", image: UIImage(named: "onb2") ?? UIImage()),
+                       OnboardingSlide(title: "Title3", description: "Description3", image: UIImage(named: "onb3") ?? UIImage())]
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+    }
+}
+
+extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.slides.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingCollectionViewCell.identifier, for: indexPath) as! OnboardingCollectionViewCell
+        
+        cell.setup(slides[indexPath.row])
+        
+        return cell
+    }
+}
+
+extension OnboardingViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+}
+
