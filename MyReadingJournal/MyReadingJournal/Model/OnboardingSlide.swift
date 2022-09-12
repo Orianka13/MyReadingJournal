5f605727bb6844792367705b42c8a0551fcc7021
Orianka13 <odobrogor@gmail.com>
2022-09-12 14:07:13 +0300

    Создана модель для Onboarding

---
+++ MyReadingJournal/MyReadingJournal/Model/OnboardingSlide.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingSlide.swift
+//  MyReadingJournal
+//
+//  Created by Олеся Егорова on 12.09.2022.
+//
+
+import UIKit
+
+struct OnboardingSlide {
+    let title: String
+    let description: String
+    let image: UIImage
+}
